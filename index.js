6813577eed6408f721aa85d5e6ebb5d52f6dbb28
kwakchanyang <king111899@naver.com>
2025-02-11 14:30:30 +0900

    localstorage added

--- index.js
+++ index.js
@@ -1,90 +1,69 @@
-// querySelector= HTML의 모든것을 선택할 수 잇음: 태그이름, 클래스명, 아이디명 모두 선택가능 선택할 때 css 규칙을 따라서 작성함
-// 예) class="newClass" => '.newClass'
-// id="newId" => '#id'
-// ul div => 'ul div'
-// ul div.newClass => 'ul div.newClass'
-// const body = document.querySelector("body")
-// body.style.backgroundColor = "coral"
-// const h1 = document.querySelector("header h1")
-// h1.style.color = "coral"
+const db = localStorage; // 문자열로 가져옴(String)
 
-// getElementById = id값만 그래서 그냥 '' 안에 적어주면됨
-// 예) id="item" => ('item')
-// document.querySelector("input").id = "item"
-// const item = document.getElementById("item")
-// item.style.border = "1px solid"
+const data = db.getItem("list");
+// null = 아무것도 없는 값 // object memory
+// undefined = 아무것도 없음
+const newData = JSON.parse(data);
 
-// innerHTML = 해당 태그 안에 새로운 자식요소를 넣을 때 사용함
-// 자식요소는 태그들 문자열에 담아서 사용해도 되지만. 변수를 같이 사용하려면 ``사용해서 하면 개꿀.
-
-let list = []
+let list = newData ?? []; // ?? 앞의 조건이 만족되지 않을 때 안전빵으로 줄 초기값
 
 const rendering = () => {
-  const ul = document.querySelector("ul")
+  const ul = document.querySelector("ul");
 
-  ul.innerHTML = null
+  ul.innerHTML = null;
 
   for (
     let i = 0;
     i < list.length;
     i = i + 1 // i += 1 // i++
   ) {
-    const button = document.createElement("button")
+    const button = document.createElement("button");
 
-    button.innerText = "삭제"
+    button.innerText = "삭제";
     button.onclick = () => {
-      list.splice(i, 1)
+      list.splice(i, 1);
 
-      rendering()
-    }
+      rendering();
+    };
 
-    const p = document.createElement("p")
-    p.innerText = list[i]
-    const div = document.createElement("div")
+    const p = document.createElement("p");
+    p.innerText = list[i];
+    const div = document.createElement("div");
 
-    div.append(p, button)
+    div.append(p, button);
 
-    const li = document.createElement("li")
-    li.append(div)
+    const li = document.createElement("li");
+    li.append(div);
 
-    ul.append(li)
-
-    //   const tag = `
-    //     <li>
-    //         <div>
-    //             <p>${list[i]}</p>
-    //             ${button}
-    //         </div>
-    //     </li>
-    //     `
-    //   console.log(tag)
-    //   number += i
-    //   li += tag
+    ul.append(li);
   }
-}
+};
 
-rendering()
+rendering();
 
-const form = document.querySelector("form")
-const input = document.getElementById("item")
+const form = document.querySelector("form");
+const input = document.getElementById("item");
 
 form.addEventListener(
   "submit",
 
   (event) => {
-    event.preventDefault() // 새로고침 방지 // form 태그 한정
+    event.preventDefault(); // 새로고침 방지 // form 태그 한정
 
-    const item = input.value
+    const item = input.value;
     if (item.length === 0) {
-      alert("장 볼 물건을 입력해주세요.")
-      return input.focus()
+      alert("장 볼 물건을 입력해주세요.");
+      return input.focus();
     }
 
     // list.push()
-    list.unshift(item)
+    list.unshift(item);
 
-    rendering()
+    console.log(list);
 
-    input.value = ""
+    db.setItem("list", JSON.stringify(list)); // list를 문자열로 압축시켜 저장하겠다.
+    rendering();
+
+    input.value = "";
   }
-)
+);
